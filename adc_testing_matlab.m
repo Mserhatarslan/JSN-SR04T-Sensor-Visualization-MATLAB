e4a161019c8c031194b16b36ef5e388347611f4f
Mehmet Serhat Arslan <63358327+Mserhatarslan@users.noreply.github.com>
2022-04-17 13:56:18 +0300

    D

---
+++ adc_testing_matlab.m
@@ -0,0 +1,27 @@
+clc
+clear all;
+delete(instrfindall);
+b=serial('COM8','BaudRate',9600);
+fopen(b);
+i=1;
+
+y=[];
+n=0;
+while(n<1000)
+  y1(i)=fscanf(b, '%d');  %ADC1 almak
+ %y2(i)=fscanf(b,'%d');
+  plot(y1,'b--','linewidth',3)
+%plot(y2,'r--','linewidth',3)
+drawnow;
+grid on;
+hold on;
+title('JSN SR04t Testing');
+xlabel('Time in seconds');
+ylabel('Distance Value');
+    n=n+1;
+    i=i+1;   
+    
+end
+fclose(b);
+
+plot(n,y1);
